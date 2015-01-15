46c151cc883cab6965ed927cbc489f7f6e2a6e84
Adrien Folie <folie.adrien@gmail.com>
2015-01-15 15:03:53 +0100

    fix config files

--- run.sh
+++ run.sh
@@ -2,16 +2,23 @@
 
 set -e
 
+# Restore config file
 if [ -f /config/config.ini ]
 then
 	rm -rf "$APP/config.ini"
-	rm -rf "$APP/sickbeard.db"
 else
 	touch /config/config.ini
+fi
+ln -sf /config/config.ini "$APP/config.ini"
+
+# Restore database backup
+if [ -f /config/sickbeard.db ]
+then
+	rm -rf "$APP/sickbeard.db"
+else
 	touch /config/sickbeard.db
 fi
-
-ln -sf /config/config.ini "$APP/config.ini"
 ln -sf /config/sickbeard.db "$APP/sickbeard.db"
 
+# Launch sickrage
 python "$APP/SickBeard.py" -q
